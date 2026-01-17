4e9750395db07b69e694e1d936629c58fa830b11
PrithviRajVerma <2266prr@gmail.com>
2026-01-17 18:28:15 +0530

    project structure designed and tailwind configuration

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-4xl font-bold text-blue-600">This is my first professional website.</h1>
+
+    </>
+
+  )
+}
+
+export default App
